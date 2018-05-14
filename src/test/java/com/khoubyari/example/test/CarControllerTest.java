7286c487bf46c68828e9352776d442ef66ed127b
Siamak Khoubyari <siamak.khoubyari@symphony.com>
2018-05-14 11:54:11 -0700

    more car service unit tests

--- src/test/java/com/khoubyari/example/test/CarControllerTest.java
+++ src/test/java/com/khoubyari/example/test/CarControllerTest.java
@@ -11,6 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.Matchers.*;
 
+import java.util.Date;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class CarControllerTest extends TestBase {
@@ -22,6 +23,10 @@ public class CarControllerTest extends TestBase {
 
     @Test
     public void shouldCreateRetrieveDelete() throws Exception {
+
+        // remove everything first
+        carRepository.deleteAll();
+
         Car r1 = mockCar("shouldCreateRetrieveDelete");
         byte[] r1Json = toJson(r1);
 
@@ -54,12 +59,57 @@ public class CarControllerTest extends TestBase {
         
     }
 
+    @Test
+    public void shouldCreateAndUpdateAndDelete() throws Exception {
+
+        // remove everything first
+        carRepository.deleteAll();
+
+        Car r1 = mockCar("shouldCreateRetrieveDelete");
+        byte[] r1Json = toJson(r1);
+
+        //CREATE
+        MvcResult result = mvc.perform(post("/example/v1/cars")
+                .content(r1Json)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(redirectedUrlPattern(RESOURCE_LOCATION_PATTERN))
+                .andReturn();
+        String id = getResourceIdFromUrl(result.getResponse().getRedirectedUrl());
+
+        Car r2 = mockCar("shouldCreateAndUpdate2");
+        r2.setId(id); // id should match when we are updating
+        byte[] r2Json = toJson(r2);
+
+        //UPDATE
+        result = mvc.perform(put("/example/v1/cars/" + id)
+                .content(r2Json)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent())
+                .andReturn();
+
+        //RETRIEVE updated
+        mvc.perform(get("/example/v1/cars/" + id)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is((id))))
+                .andExpect(jsonPath("$.name", is(r2.getName())))
+                .andExpect(jsonPath("$.year", is(r2.getYear())));
+
+        //DELETE
+        mvc.perform(delete("/example/v1/cars/" + id))
+                .andExpect(status().isNoContent());
+    }
+
+
     private Car mockCar(String prefix) {
         int min = 1900;
         int max = 2019;
         Car r = new Car();
         r.setYear(ThreadLocalRandom.current().nextInt(min, max + 1));
-        r.setName(prefix + "_name");
+        r.setName(prefix + "_" + new Date().getTime());
         return r;
     }
 
