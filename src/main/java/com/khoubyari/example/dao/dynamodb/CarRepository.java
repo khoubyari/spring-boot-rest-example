3da6f8c1e6220a364ce83b79c934a84db3a1beb1
Siamak Khoubyari <khoubyari@gmail.com>
2018-05-24 14:01:20 -0700

    add findAll() to return list with pagination support

--- src/main/java/com/khoubyari/example/dao/dynamodb/CarRepository.java
+++ src/main/java/com/khoubyari/example/dao/dynamodb/CarRepository.java
@@ -2,14 +2,17 @@ package com.khoubyari.example.dao.dynamodb;
 
 import com.khoubyari.example.domain.Car;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
-import org.springframework.data.repository.CrudRepository;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScanCount;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @EnableScan
+@EnableScanCount  // ok for small collections / low volume services. otherwise research the performance implications
 @Repository
-public interface CarRepository extends CrudRepository<Car, String> {
+public interface CarRepository extends PagingAndSortingRepository<Car, String> {
 
-    List<Car> findByName(String lastName);
+    Page findAll(Pageable pageable);
+
 }
