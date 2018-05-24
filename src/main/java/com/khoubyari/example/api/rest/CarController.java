6a7ba4acb0e1a87c2b5352184b4f4190ede82644
Siamak Khoubyari <khoubyari@gmail.com>
2018-05-24 13:59:55 -0700

    add GET /car  (returns a paginated list)

--- src/main/java/com/khoubyari/example/api/rest/CarController.java
+++ src/main/java/com/khoubyari/example/api/rest/CarController.java
@@ -9,6 +9,7 @@ import com.khoubyari.example.exception.DataFormatException;
 import com.khoubyari.example.service.CarService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,11 +35,11 @@ public class CarController extends AbstractRestHandler {
     @ResponseStatus(HttpStatus.CREATED)
     @ApiOperation(value = "Create a car resource.", notes = "Returns the URL of the new resource in the Location header.")
     public void createCar(@RequestBody Car car,
-                            HttpServletRequest request, HttpServletResponse response) {
+                          HttpServletRequest request, HttpServletResponse response) {
         Car createdCar = this.carService.createCar(car);
         response.setHeader("Location", request.getRequestURL().append("/").append(createdCar.getId()).toString());
     }
-/*
+
     @RequestMapping(value = "",
             method = RequestMethod.GET,
             produces = {"application/json", "application/xml"})
@@ -47,13 +48,13 @@ public class CarController extends AbstractRestHandler {
     public
     @ResponseBody
     Page<Car> getAllCar(@ApiParam(value = "The page number (zero-based)", required = true)
-                            @RequestParam(value = "page", required = true, defaultValue = DEFAULT_PAGE_NUM) Integer page,
-                            @ApiParam(value = "Tha page size", required = true)
-                            @RequestParam(value = "size", required = true, defaultValue = DEFAULT_PAGE_SIZE) Integer size,
-                            HttpServletRequest request, HttpServletResponse response) {
+                        @RequestParam(value = "page", required = true, defaultValue = DEFAULT_PAGE_NUM) Integer page,
+                        @ApiParam(value = "Tha page size", required = true)
+                        @RequestParam(value = "size", required = true, defaultValue = DEFAULT_PAGE_SIZE) Integer size,
+                        HttpServletRequest request, HttpServletResponse response) {
         return this.carService.getAllCars(page, size);
     }
-*/
+
     @RequestMapping(value = "/{id}",
             method = RequestMethod.GET,
             produces = {"application/json", "application/xml"})
@@ -62,8 +63,8 @@ public class CarController extends AbstractRestHandler {
     public
     @ResponseBody
     Car getCar(@ApiParam(value = "The ID of the car.", required = true)
-                   @PathVariable("id") String id,
-                   HttpServletRequest request, HttpServletResponse response) throws Exception {
+               @PathVariable("id") String id,
+               HttpServletRequest request, HttpServletResponse response) throws Exception {
         Car car = this.carService.getCar(id);
         checkResourceFound(car);
         //todo: http://goo.gl/6iNAkz
@@ -77,10 +78,10 @@ public class CarController extends AbstractRestHandler {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @ApiOperation(value = "Update a car resource.", notes = "You have to provide a valid car ID in the URL and in the payload. The ID attribute can not be updated.")
     public void updateCar(@ApiParam(value = "The ID of the existing car resource.", required = true)
-                            @PathVariable("id") String id, @RequestBody Car car,
-                            HttpServletRequest request, HttpServletResponse response) {
+                          @PathVariable("id") String id, @RequestBody Car car,
+                          HttpServletRequest request, HttpServletResponse response) {
         checkResourceFound(this.carService.getCar(id));
-        if (! id.equals(car.getId())) throw new DataFormatException("ID doesn't match!");
+        if (!id.equals(car.getId())) throw new DataFormatException("ID doesn't match!");
         this.carService.updateCar(car);
     }
 
@@ -91,8 +92,8 @@ public class CarController extends AbstractRestHandler {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @ApiOperation(value = "Delete a car resource.", notes = "You have to provide a valid car ID in the URL. Once deleted the resource can not be recovered.")
     public void deleteCar(@ApiParam(value = "The ID of the existing car resource.", required = true)
-                            @PathVariable("id") String id, HttpServletRequest request,
-                            HttpServletResponse response) {
+                          @PathVariable("id") String id, HttpServletRequest request,
+                          HttpServletResponse response) {
         checkResourceFound(this.carService.getCar(id));
         this.carService.deleteCar(id);
     }
