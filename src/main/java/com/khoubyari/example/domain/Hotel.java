2baf2f0b4e237e2e347008d2c7b0311d3cbd2e97
Ishmeet Singh <76062864+IshmeetSingh06@users.noreply.github.com>
2022-09-16 00:09:45 +0530

    Added Lombok Annotations
    Added the Lombok annotations to remove the boilerplate code.

--- src/main/java/com/khoubyari/example/domain/Hotel.java
+++ src/main/java/com/khoubyari/example/domain/Hotel.java
@@ -7,6 +7,10 @@ import javax.xml.bind.annotation.*;
  * a simple domain entity doubling as a DTO
  */
 @Entity
+@Data                                         //For getters,setters,hashcode and toString methods 
+@NoArgsConstructor                            //For No argument Constructor
+@AllArgsConstructor                           //For All arguments Constructor
+@Builder                                      //Implemented Builder Pattern
 @Table(name = "hotel")
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -27,65 +31,4 @@ public class Hotel {
 
     @Column()
     private int rating;
-
-    public Hotel() {
-    }
-
-    public Hotel(String name, String description, int rating) {
-        this.name = name;
-        this.description = description;
-        this.rating = rating;
-    }
-
-    public long getId() {
-        return this.id;
-    }
-
-    // for tests ONLY
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    @Override
-    public String toString() {
-        return "Hotel {" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", city='" + city + '\'' +
-                ", rating=" + rating +
-                '}';
-    }
 }
